251d552e5782ff8aa176093d78e6d3eabd72369f
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-03-20 00:02:04 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
     <React.StrictMode>
         <App />
