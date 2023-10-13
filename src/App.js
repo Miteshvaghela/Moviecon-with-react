4a6aea79a504b1f6600afdb23a7eec778253ef54
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-10-13 16:47:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Movies from './components/Movies';
 import SearchBar from './components/SearchBar';
 import MovieForm from './components/MovieForm'; 
 
+
 const App = () => { 
   const [showForm, setShowForm] = useState(false);
   const [term, setTerm] = useState('');
