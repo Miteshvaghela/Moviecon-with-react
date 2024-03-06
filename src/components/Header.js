eefd535fef785fba7d01e370356807422db194d7
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-03-06 16:51:12 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
-import React from 'react'; 
+import React from 'react';
 
-export default Header = ({title}) => {
+const Header = ({ title }) => {
     return (
-        <header className="header">
+        <div className="header">
             <div className="logo"><h1>{title}</h1></div>
             <nav className="nav menu">
-                <a className="menu-item" href="#">Home</a> 
+                <a className="menu-item" href="/">Home</a> 
             </nav>
-        </header>
+        </div>
     )
 }
+
+export default Header;
--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const MovieForm = ({addMovie}) => {
     const [title, setTitle] = useState('');
